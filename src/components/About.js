3592b6a6ee3851d2880325a349b0f6acb81a159f
Eric McKee <lyolayale19@gmail.com>
2023-12-28 22:43:03 -0600

    Created about component/styled.

---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+export default function About() {
+  return (
+    <section
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white px-4"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <section className="pb-10">
+          <p className="text-5xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </section>
+        <p className="text-xl mt-20">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo
+          ipsam quo fuga et ea consectetur maxime amet obcaecati nam deleniti
+          suscipit beatae, optio distinctio mollitia asperiores quos culpa
+          nobis! Eius quod asperiores harum nostrum non reiciendis delectus
+          alias culpa reprehenderit eaque consequatur quos dolorem, porro
+          consectetur sed. Blanditiis, consequatur?
+        </p>
+        <br />
+        <p className="text-xl">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, ab
+          voluptatibus impedit expedita illo harum necessitatibus vitae! Magni,
+          eos voluptate? Tenetur unde molestias dolorum totam a iste placeat
+          enim quidem quibusdam nulla dolorem eum non deleniti quo suscipit rem
+          repellat sunt, quia adipisci, perferendis possimus delectus sequi
+          saepe recusandae? Suscipit?
+        </p>
+      </div>
+    </section>
+  );
+}
