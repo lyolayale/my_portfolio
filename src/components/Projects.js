f5a69dab622192a3fc048c24246e0ac26c2c0de6
Eric McKee <lyolayale19@gmail.com>
2024-01-21 14:29:02 -0600

    Updated portfolio section -> added project & added styling.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import airBnb from "../assets/portfolio/air-bnb.png";
+import splitBill from "../assets/portfolio/split-bill.png";
 import githubGallery from "../assets/portfolio/github-gallery - Edited.png";
 import guessWord from "../assets/portfolio/guess-the-word-game - Edited.png";
 import meme from "../assets/portfolio/meme.png";
@@ -9,39 +9,45 @@ export default function Portfolio() {
   const portfolio = [
     {
       id: crypto.randomUUID(),
-      src: airBnb,
-      demo: "https://lyolayale.github.io/mini-airbnb-clone/",
-      code: "https://github.com/lyolayale/mini-airbnb-clone",
+      src: splitBill,
+      demo: " https://lyolayale.github.io/split-bill-frontend/",
+      code: "https://github.com/lyolayale/split-bill-frontend",
+      title: "Split the Bill",
     },
     {
       id: crypto.randomUUID(),
       src: githubGallery,
       demo: "https://lyolayale.github.io/github-repo-gallery/",
       code: "https://github.com/lyolayale/github-repo-gallery",
+      title: "GitHub Gallery",
     },
     {
       id: crypto.randomUUID(),
       src: guessWord,
       demo: "https://lyolayale.github.io/guess-the-word/",
       code: "https://github.com/lyolayale/guess-the-word",
+      title: "Guess the Word",
     },
     {
       id: crypto.randomUUID(),
       src: meme,
       demo: "https://lyolayale.github.io/meme-generator/",
       code: "https://github.com/lyolayale/meme-generator",
+      title: "Meme Generator",
     },
     {
       id: crypto.randomUUID(),
       src: noteCard,
       demo: "https://lyolayale.github.io/note_card/",
       code: "https://github.com/lyolayale/note_card",
+      title: "Note Cards",
     },
     {
       id: crypto.randomUUID(),
       src: tenzie,
       demo: "https://lyolayale.github.io/tenzie/",
       code: "https://github.com/lyolayale/tenzie",
+      title: "Tenzie",
     },
   ];
   return (
@@ -60,11 +66,12 @@ export default function Portfolio() {
         </div>
 
         <article className="card grid sm:grid-cols-2 md:grid-cols-3 gap-12 px-12 sm:px-0">
-          {portfolio.map(({ id, src, demo, code }) => (
+          {portfolio.map(({ id, src, demo, code, title }) => (
             <div
               key={id}
-              className="rounded-lg hover:shadow-md hover:shadow-gray-500 hover:scale-105 duration-200"
+              className="group rounded-lg hover:shadow-md hover:shadow-gray-500 hover:scale-105 duration-200"
             >
+              {/* <h3 className="text-xl text-slate-50 mb-3 p-1">{title}</h3> */}
               <a href={demo} target="_blank" rel="noreferrer">
                 <img
                   src={src}
@@ -90,6 +97,11 @@ export default function Portfolio() {
                   Code
                 </a>
               </div>
+              <h3 className="hidden text-center text-xl text-slate-50 font-bold mb-3 p-1 group-hover:block hover:scale-110 hover:text-gray-500 duration-200 hover:font-bold hover:underline">
+                <a href={demo} target="_blank" rel="noreferrer">
+                  {title}
+                </a>
+              </h3>
             </div>
           ))}
         </article>
